3a51aa11b88278515d667d4daffc85b746e1b8bb
TenthEdict <schipp0@purdue.edu>
2026-01-02 19:05:26 -0500

    1110 in rust

---
+++ Easy/Rust/1110.rs
@@ -0,0 +1,28 @@
+// Daily Coding Problem: 1110
+// Difficulty: Easy
+// Asked by: Netflix
+// Authored by: TenthEdict
+
+// Question: Given an array of integers, determine whether it contains a Pythagorean triplet.
+// Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c 2.
+
+use std::collections::HashSet;
+
+fn contains_pythagorean_triplet(arr: &Vec<i32>) -> bool {
+    let squared: HashSet<i32> = arr.iter().map(|x| x.pow(2)).collect();  
+
+    for i in &squared {
+        for j in &squared {
+            if squared.contains(&(i+j)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fn main() {
+    println!("{}", contains_pythagorean_triplet(&vec![1, 2, 3]));    // false
+    println!("{}", contains_pythagorean_triplet(&vec![5, 12, 13]));  // true
+    println!("{}", contains_pythagorean_triplet(&vec![]));           // false
+}
